5b088e6ebb026e9b830c897694332442b6989a51
오동웅 <woong@MacPro.local>
2021-05-08 03:55:27 +0900

    Add to text form on Home component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
+  const [message, setMessage] = useState<string>("");
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange: React.ChangeEventHandler<HTMLElement> = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { value },
+    } = event;
+    setMessage(value);
+  };
+
   return (
     <>
-      <h1>Home</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Please you enter message"
+        />
+        <input type="submit" value={message} />
+      </form>
     </>
   );
 };
