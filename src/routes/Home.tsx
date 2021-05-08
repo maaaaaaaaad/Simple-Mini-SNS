81f70c33e697d7b6e48a2c42051e51e68d774499
오동웅 <woong@MacPro.local>
2021-05-08 18:59:13 +0900

    Add to fireStore.collection.add

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { firebaseStore } from "../service/firebaseSet";
 
 const Home: React.FC = () => {
   const [message, setMessage] = useState<string>("");
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
+    await firebaseStore.collection("user").add({
+      text: message,
+      createAt: Date.now(),
+    });
+    formRef.current!.reset();
+    setMessage("");
   };
 
   const onChange: React.ChangeEventHandler<HTMLElement> = (
@@ -18,13 +26,15 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <form onSubmit={onSubmit}>
+      <form ref={formRef} onSubmit={onSubmit}>
         <input
+          value={message}
           onChange={onChange}
           type="text"
           placeholder="Please you enter message"
+          maxLength={300}
         />
-        <input type="submit" value={message} />
+        <input type="submit" value="Upload" />
       </form>
     </>
   );
